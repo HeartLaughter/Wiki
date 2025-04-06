fe8379105f875dbbb4df253d958557b0031df1ba
HeartLaughter <1160450743@qq.com>
2025-04-06 19:30:33 +0200

    2-9 hot deployment finished

--- src/main/java/com/micien/wiki/controller/TestController.java
+++ src/main/java/com/micien/wiki/controller/TestController.java
@@ -1,14 +1,17 @@
 package com.micien.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${mytest.hello}")
+    private String private_hello;
 
     @RequestMapping("/hello")
     public String hello(){
-        return "hello";
+        return "hello"+private_hello;
     }
 
     @RequestMapping("/hello/post")
