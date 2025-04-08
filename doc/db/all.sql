6a64389467ba7ee4ee662127ce53163e869ab4ae
HeartLaughter <1160450743@qq.com>
2025-04-08 10:34:14 +0200

    3-3 Add all.sql script

---
+++ doc/db/all.sql
@@ -0,0 +1,7 @@
+drop table if exists `test`;
+create table `test` (
+                        `id` bigint not null comment 'id',
+                        `name` varchar(50) comment 'name of user',
+                        'password' varchar(50) comment 'pwd of user',
+                        primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='Test purpose';
