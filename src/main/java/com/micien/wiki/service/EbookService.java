156c0a9989d1d557e03c9f3ad66780342b7b5dd8
HeartLaughter <1160450743@qq.com>
2025-04-10 10:06:05 +0200

    3-8 Encapsulate Copy Utils to copy single object or list.

--- src/main/java/com/micien/wiki/service/EbookService.java
+++ src/main/java/com/micien/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.micien.wiki.domain.EbookExample;
 import com.micien.wiki.mapper.EbookMapper;
 import com.micien.wiki.req.EbookReq;
 import com.micien.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.micien.wiki.util.CopyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,12 +22,15 @@ public class EbookService {
         criteria.andNameLike("%"+req.getName()+"%");
         List<Ebook> ebooklist = EbookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respslist = new ArrayList<>();
+/*        List<EbookResp> respslist = new ArrayList<>();
         for (Ebook ebook : ebooklist) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
+*//*            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook, ebookResp);*//*
+            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
             respslist.add(ebookResp);
-        }
-        return respslist;
+        }*/
+
+        List<EbookResp> ebookResps = CopyUtil.copyList(ebooklist, EbookResp.class);
+        return ebookResps;
     }
 }
